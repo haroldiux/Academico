0d9972478f1b59b69481f7c7f75cf98fc7378e11
juanjo2702 <juanjo270201@gmail.com>
2026-02-03 01:56:15 -0400

    ULTIMOS CAMBIOS

---
+++ dist/spa/assets/dashboardService-CrdezydF.js
@@ -0,0 +1,1 @@
+import{ag as a}from"./index-BJgxmIt4.js";const s=async()=>(await a.get("/admin/stats")).data,e=async t=>(await a.get("/admin/dashboard/director",{params:t})).data,o={getAdminStats:s,getDirectorStats:e};export{o as d};
--- dist/spa/assets/pinia-BjxZ4Py-.js
+++
@@ -1,1 +0,0 @@
-import{b as o,p as i}from"./index-DmFonbUr.js";const s=o(({app:a})=>{a.use(i)});export{s as default};
